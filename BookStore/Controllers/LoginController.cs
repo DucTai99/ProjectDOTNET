bdc5c4e248fe883abd614c419edb40ab60893287
DucTai99 <taihandsomeno1@gmail.com>
2020-07-21 22:50:25 +0700

    create client ui

---
+++ BookStore/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SingleProduct()
+        {
+            return View();
+        }
+    }
+}
