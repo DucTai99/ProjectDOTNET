e1819887e550d08d6362e053ecdf15d7931b29cf
hoaitu <hoaitugl@gmail.com>
2020-08-01 11:09:21 +0700

    Hiển thị chi tiết nội dung

--- BookStore/Controllers/ShopController.cs
+++ BookStore/Controllers/ShopController.cs
@@ -1,22 +1,47 @@
-﻿using System;
+﻿using Model.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using Model.EF;
 
 namespace BookStore.Controllers
 {
     public class ShopController : Controller
     {
         // GET: Shop
-        public ActionResult Index()
+        public ActionResult Index(int pageNum = 1, int pageSize = 9)
         {
+            var bookDao = new BooksDao();
+            var model = bookDao.listAllBookWithPaging(pageNum, pageSize);
+            ViewData["ListBookType"] = new BookTypeDao().getAllType();
+            return View(model);
+        }
+
+        //Detail
+        public ActionResult SingleProduct(int id)
+        {
+            var bookDao = new BooksDao().bookWithID(id);
+            ViewData["book"] = bookDao;
+            //Cùng thể loại
+            ViewData["sameBook"] = new BooksDao().sameCate(id);
+
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (bookDao == null)
+            {
+                return HttpNotFound();
+            }
+
             return View();
         }
 
-        public ActionResult SingleProduct()
-        {
-            return View();
-        }
     }
 }
+
+                                           
---
+++ Model/DAO/BooksDao.cs
@@ -0,0 +1,49 @@
+﻿using Model.EF;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+
+namespace Model.DAO
+{
+    public class BooksDao
+    {
+        private BookStoreDbContext db;
+        public BooksDao()
+        {
+            this.db = new BookStoreDbContext();
+        }
+
+        public List<sach> getAllBook()
+        {
+            List<sach> listBook = new List<sach>();
+            listBook = db.saches.ToList();
+
+            return listBook;
+        }
+
+        public IEnumerable<sach> listAllBookWithPaging(int pageNum, int pageSize)
+        {
+            return db.saches.OrderBy(sach => sach.tenSach).ToPagedList(pageNum, pageSize);
+        }
+        ///
+
+        public sach bookWithID(int id)
+        {
+            return db.saches.Find(id);
+        }
+
+        public List<sach> sameCate (int id)
+        {
+            var cate = db.saches.Find(id);
+            List<sach> listBook = new List<sach>();
+            //khác id sách , nhưng cùng mã thể loại
+            listBook = db.saches.Where(book => book.maSach != id && book.loaiSach == cate.loaiSach).ToList();
+            return listBook;
+        }
+
+        }
+}
