072a7436070fd8527904607c305f65e591635f2c
DucTai99 <taihandsomeno1@gmail.com>
2020-08-12 11:42:14 +0700

    finish comment with ajax

---
+++ Model/DAO/CommentDao.cs
@@ -0,0 +1,24 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class CommentDao
+    {
+        private BookStoreDbContext db;
+        public CommentDao()
+        {
+            this.db = new BookStoreDbContext();
+        }
+
+        public void addComment(comment comment)
+        {
+            db.comments.Add(comment);
+            db.SaveChanges();
+        }
+    }
+}
